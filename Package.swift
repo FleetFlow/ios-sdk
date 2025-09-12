10137d716655e56aa227d06ab61f834f2cbd6e7c
Quinten Adema <quinten@ademagroup.com>
2025-09-12 23:37:05 +0200

    Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "FleetFlowSDK",
+    platforms: [
+		.iOS(.v14)
+	],
+    products: [
+        .library(name: "FleetFlowSDK", targets: ["FleetFlowSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "FleetFlowSDK",
+            url: "https://github.com/FleetFlow/ios-sdk/releases/download/v0.0.0/FleetFlowSDK.xcframework.zip",
+            checksum: "REPLACED_BY_CI"
+        )
+    ]
+)
